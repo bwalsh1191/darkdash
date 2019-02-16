cc0ad85b752dfcc8c1131ddaba9b0ca433252dd4
Brian <bwalshgogiants@gmail.com>
2019-02-16 15:50:32 -0500

    Working Basis
    working basis for flask/css/etc...

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_name():
-   user="Worldffff"
-   return render_template('hello.html', name = user)
+   user="Google"
+   return render_template('hello.html', chart_name = user)
 
 if __name__ == '__main__':
    app.run(debug = True)
