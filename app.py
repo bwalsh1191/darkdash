d521b67bc50d9e83cf9912eb8c9756711ec9973f
Brian <bwalshgogiants@gmail.com>
2019-02-16 23:46:11 -0500

    Working
    Working with 1 chart. may break so want to establish working point

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     years = [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050]
-    africa = [86,114,106,106,107,111,133,221,783,2478]
-    colors = ['#ff0000','#0000ff','#ffffe0','#008000','#800080','#FFA500']
-    return render_template('index.html', values=years, labels=africa, colors=colors)
+    values = [86,114,106,106,107,111,133,221,783,2478]
+    
+    return render_template('index.html', values=values, labels=years)
 
 if __name__ == '__main__':
     app.run(debug = True)
