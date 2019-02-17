0389cae8004c03141d4c1492f86bacc0386c52a4
Brian <bwalshgogiants@gmail.com>
2019-02-17 00:29:52 -0500

    API Works
    pulling stock data manually. still buggy though

--- app.py
+++ app.py
@@ -1,13 +1,41 @@
 from flask import Flask, render_template
+import pandas as pd
 app = Flask(__name__)
+
+API_KEY = '4HNDQOUQ2A1G90RW'
 
 @app.route('/')
 def hello_world():
-    years = [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050]
-    values = [86,114,106,106,107,111,133,221,783,2478]
-    values2 = [2478,783,123,445,320,111,133,221,783,86]
-    
-    return render_template('index.html', values=values, years=years, values2 = values2)
 
+    symbol='msft'
+    values, years, days = make_df(symbol)
+    
+    return render_template('index.html', values=values, years=years, symbol=symbol, days=days)
+'''
+def get_data():
+    years = [2000,2001,2002,2003,2004,2005,2006,2007,2008,2009]
+    values = [86,114,106,106,107,111,133,221,783,2478]
+    return(years, values)
+'''
+
+def make_df(symbol): 
+    #use this one for testing
+    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=' + symbol + '&outputsize=compact&datatype=csv&apikey=' + API_KEY
+    df = pd.read_csv(url, index_col="timestamp", parse_dates = True, na_values = ' ')
+    df['close'] = df['adjusted_close']
+    df.index.names = ['date']
+
+    #mod df starts here
+    df['date'] = df.index
+    df['day'] = df['date'].dt.day_name()
+    df['month'] = df['date'].dt.month_name()
+    df['year'] = df['date'].dt.year
+    close_price = df['close'].tolist()
+    date_list = df['date'].tolist()
+    days= df['month'].tolist()
+    return(close_price, date_list, days)
+
+
+#THIS HAST TO BE THE END OF THE FILE
 if __name__ == '__main__':
     app.run(debug = True)
